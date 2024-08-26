3d58b221cc1f0f8290db364ff486c2658e06c169
Lev Solodko <46357405+o4dushno@users.noreply.github.com>
2024-08-26 15:54:33 +0300

    Оптимизация запросов

--- src/chats/crud.py
+++ src/chats/crud.py
@@ -20,7 +20,6 @@ async def create_private_chat_crud(
     new_chat.users.extend(users)
     db.add(new_chat)
     await db.commit()
-    await db.refresh(new_chat)
     return new_chat, new_conversation
 
 
@@ -43,14 +42,17 @@ async def users_private_chat_exists(db, users: List[User]) -> bool:
     return exists_
 
 
-async def public_chat_exists(db, name: str):
+async def public_chat_exists(db, name: str) -> PublicChat:
     result = await db.execute(
         select(exists().where(PublicChat.name == name))
     )
     return result.scalars().first()
 
 
-async def create_public_chat_crud(db, name: str, owner_id: UUID):
+async def create_public_chat_crud(
+    db, name: str, owner_id: UUID
+) -> Tuple[PublicChat, Conversation]:
+
     new_conversation = Conversation(is_group=True)
     db.add(new_conversation)
     await db.flush()
@@ -62,12 +64,10 @@ async def create_public_chat_crud(db, name: str, owner_id: UUID):
     new_public_chat.conversations.append(new_conversation)
     db.add(new_public_chat)
     await db.commit()
-    await db.refresh(new_public_chat)
-    await db.refresh(new_conversation)
     return new_public_chat, new_conversation
 
 
-async def get_public_chat_by_uuid(db, chat_id: UUID):
+async def get_public_chat_by_uuid(db, chat_id: UUID) -> PublicChat:
     public_chat = await db.execute(
         select(PublicChat)
         .options(joinedload(PublicChat.users))
@@ -78,7 +78,7 @@ async def get_public_chat_by_uuid(db, chat_id: UUID):
 
 async def enjoy_user_to_public_chat(
     db, public_chat: PublicChat, current_user: User
-):
+) -> None:
     public_chat.users.append(current_user)
     db.add(public_chat)
     await db.commit()
